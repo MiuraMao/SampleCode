0f171a0fd6a95f4815afcf5a3773e0cbcf6efe1b
MiuraMao <m_miura06@outlook.com>
2020-05-09 21:22:40 +0900

    2st commit

---
+++ kuku.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int i, j;                  /*九九の数字を表す整数*/
+    for(j = 0; j < 9, j++;) {
+        printf("%dの段", j+1);
+        for(i = 0; i < 9; i++){/*１段分を表示する*/
+                printf("%d×%d=%2d", j+1, i+1, (j+1)*(i+1));
+        }
+            printf("\n");
+    }
+    return 0;
+}
